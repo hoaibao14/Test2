31965b234e06cd89fb2e5d02bd4beeef449d27fd
hoaibao14 <lahoaibao14@gmail.com>
2017-10-26 19:08:16 +0700

    first

---
+++ DiagonalDifference/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace DiagonalDifference
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(53, 6);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(243, 109);
+            this.tbInput.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Result";
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(53, 150);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(243, 20);
+            this.tbResult.TabIndex = 3;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(53, 121);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(72, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(380, 192);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ DiagonalDifference/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DiagonalDifference
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string input = tbInput.Text;
+            string[] lines = input.Split(new Char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = int.Parse(lines[0]);
+            if (n == (lines.Count() - 1))
+            {
+                int[,] multiArray = new int[n, n];
+                for (int i = 1; i <= n; i++)
+                {
+                    string[] line = lines[i].Split(' ');
+                    for (int j = 0; j < line.Count(); j++)
+                    {
+                        if ((int.Parse(line[j]) >= -100) && (int.Parse(line[j]) <= 100))
+                        {
+                            multiArray[i-1,j] = int.Parse(line[j]);
+                        }
+                        else {
+                            tbResult.Text = "Value of element is invalid!";
+                            return;
+                        }
+                        
+                    }
+                }
+
+                int sum1=0, sum2=0;
+
+                for (int i = 0; i < n; i++)
+                {
+                        sum1 += multiArray[i, i];
+                        sum2 += multiArray[i, n-i-1];
+                }
+                tbResult.Text = Math.Abs(sum1-sum2).ToString() + " = "+ sum1.ToString() + "-" + sum2.ToString();
+
+            }
+
+        }
+
+    }
+}
